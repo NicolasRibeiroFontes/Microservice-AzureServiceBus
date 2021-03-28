410f3b71789fec55839b13c434458f18a273fffc
Nicolas Fontes <nicolas.rfontes@gmail.com>
2021-03-28 14:30:01 +0100

    Requester Working

---
+++ Microservice-AzureServiceBus/Controllers/ProductsController.cs
@@ -0,0 +1,43 @@
+﻿using Microservice_AzureServiceBus.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Extensions.Configuration;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Microservice_AzureServiceBus.Controllers
+{
+    [Route("api/[controller]"), ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IConfiguration config;
+        private readonly string connectionString;
+
+        public ProductsController(IConfiguration config)
+        {
+            this.config = config;
+            connectionString = this.config.GetValue<string>("AzureServiceBus");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Product product)
+        {
+            await SendMessageQueue(product);
+            return Ok(product);
+        }
+
+
+        private async Task SendMessageQueue(Product product)
+        {
+            string queueName = "product";
+            var client = new QueueClient(connectionString, queueName, ReceiveMode.PeekLock);
+            string messageBody = JsonSerializer.Serialize(product);
+            var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+            await client.SendAsync(message);
+            await client.CloseAsync();
+        }
+
+    }
+}
---
+++ Microservice-AzureServiceBus/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Microservice_AzureServiceBus.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
